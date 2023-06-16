3c07ddaa2d52f2f5bfb163a24a68798640bc1d73
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-06-16 11:42:52 -0600

    Fix camera rotation

--- examples/util/camera.rs
+++ examples/util/camera.rs
@@ -9,6 +9,7 @@ pub struct Camera {
     right_dir: Vec3,
     up_dir: Vec3,
     pitch: f32,
+    yaw: f32,
 }
 
 impl Camera {
@@ -19,22 +20,25 @@ impl Camera {
             persp_matrix: Mat4::perspective_rh(fov, aspect_ratio, near, far),
             view_matrix: Mat4::IDENTITY,
             world_pos,
-            rot_matrix: Mat3A::from_euler(glam::EulerRot::XYZ, 0.0, 0.0, 0.0),
+            rot_matrix: Mat3A::from_euler(glam::EulerRot::YXZ, 0.0, 0.0, 0.0),
             look_dir,
             right_dir,
             up_dir: look_dir.cross(right_dir).normalize(),
             pitch: 0.0,
+            yaw: 0.0,
         }
     }
 
-    pub fn rotate(&mut self, mut pitch: f32, yaw: f32) {
-        if self.pitch + pitch >= 89.9f32.to_radians() {
-            pitch = 0.0;
-        }
+    pub fn rotate(&mut self, pitch: f32, yaw: f32) {
         self.pitch += pitch;
-        self.rot_matrix = Mat3A::from_euler(glam::EulerRot::XYZ, pitch, yaw, 0.0);
-        self.look_dir = self.rot_matrix * self.look_dir;
-        self.right_dir = Vec3::new(0.0, 1.0, 0.0).cross(self.look_dir).normalize();
+        self.yaw += yaw;
+        self.pitch = self
+            .pitch
+            .clamp(-89.0f32.to_radians(), 89.0f32.to_radians());
+
+        self.rot_matrix = Mat3A::from_euler(glam::EulerRot::YXZ, self.yaw, self.pitch, 0.0);
+        self.look_dir = (self.rot_matrix * Vec3::NEG_Z).normalize();
+        self.right_dir = Vec3::Y.cross(self.look_dir).normalize();
         self.up_dir = self.look_dir.cross(self.right_dir).normalize();
     }
 
@@ -44,7 +48,7 @@ impl Camera {
     }
 
     pub fn tick(&mut self) {
-        self.view_matrix = glam::Mat4::look_to_rh(self.world_pos, self.look_dir, self.up_dir);
+        self.view_matrix = glam::Mat4::look_to_rh(self.world_pos, self.look_dir, Vec3::Y);
     }
 
     pub fn view_projection_matrix(&self) -> Mat4 {
